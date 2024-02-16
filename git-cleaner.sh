cc0215398a1f76ae3ce153efc16c1fffb9e60d94
Eugene Zimin <eugene@ziminpro.com>
2024-02-15 22:07:13 -0800

    added some checks
    - made input checker
    - added file existence checker
    
    Signed-off-by: Eugene Zimin <eugene@ziminpro.com>

--- git-cleaner.sh
+++ git-cleaner.sh
@@ -12,9 +12,8 @@ if ! command -v tput >/dev/null; then
 fi
 
 # Check for bfg file
-bfg_file=$(find . -name "bfg*" -print -quit)
-
-if [[ -z "$bfg_file" ]]; then
+# Check for files starting with "bfg" using ls and grep
+if [[ ! $(ls | grep "^bfg") ]]; then
   echo "Error: No 'BFG-Cleaner' found in the local folder."
   exit 1
 fi
@@ -22,13 +21,39 @@ fi
 echo "######################################################"
 echo "#              Git Repo History Cleaner              #"
 echo "######################################################"
+
 echo
-echo -n "Enter filename with path: " && read -p "" filename
+echo -n "Enter path to repo to clean (absolute): " && read -p "" repo
+
+# Check if folder exists
+if [ ! -d "$repo" ]; then
+  echo "Error: Folder '$repo' does not exist."
+  exit 1
+fi
+
+# Check for .git directory
+if [ ! -d "$repo/.git" ]; then
+  echo "Error: '$repo' is not a git repository."
+  exit 1
+fi
+
+echo -n "Enter the filename to delete (from repo's root): " && read -p "" filename
+# Check if the filename is empty
+if [[ -z "$filename" ]]; then
+  echo "Error: Please enter a valid filename."
+  exit 1
+fi
+
 echo "The following file will be deled: $filename"
 echo
 
+echo -n "Do you want to continue? (y/n): "
 while true; do
-  read -n 1 -t 60 -p "Do you want to continue? (y/n): " answer
+  # Setting curson after the question
+  tput hpa 32
+  
+  # Waiting for input string
+  read -n 1 -t 60 -p "" answer
   case "$answer" in
     y)
       echo
@@ -41,21 +66,13 @@ while true; do
       exit 0
       ;;
     *)
-      # Get terminal dimensions
-      LINES=$(tput lines)
-      COLS=$(tput cols)
-
-      # Move cursor to the last line, first column
+      # Move cursor one symbol left and 
+      # clean up till the end of the string
+      tput cub1
       tput el
-      tput cup $((LINES - 1)) 0
       ;;
   esac
 done
 
-# Check if the filename is empty
-if [[ -z "$filename" ]]; then
-  echo "Error: Please enter a valid filename."
-  exit 1
-fi
 
 
